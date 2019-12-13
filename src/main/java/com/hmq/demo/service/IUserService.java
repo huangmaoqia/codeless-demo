3c78d749e9eba863d9c1023bcb27a9da1b7bce2f
huangmaoqia <349805562@qq.com>
2019-12-13 17:48:51 +0800

    优化

--- src/main/java/com/hmq/demo/service/IUserService.java
+++ src/main/java/com/hmq/demo/service/IUserService.java
@@ -1,8 +1,11 @@
 package com.hmq.demo.service;
 
+import java.util.List;
+
 import com.hmq.demo.model.po.User;
 import com.hmq.demo.model.vo.UserVO;
 import com.hmq.framework.service.IGenViewService;
 
 public interface IUserService extends IGenViewService<UserVO,User,String>{
+	public List<User> tt();
 }
